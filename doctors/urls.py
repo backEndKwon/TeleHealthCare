2f796c0f4d4ee8640df8a99d264290cf916c8760
backEndKwon <rhaehfdl0433@gmail.com>
2023-11-11 01:00:06 +0900

    feat: 의사 문자열 검색 1차 완료

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from django.urls import path, include
 from . import views
+from .views import doctorViewSet
 
 router = routers.DefaultRouter()
 
 router.register('doctors', views.doctorViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('search/', doctorViewSet.as_view({'get':'searchDoctor'}), name='searchDoctor')
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from .models import Doctor
 from rest_framework import viewsets
 from .serializers import DoctorSerializer
-
+from django.utils.dateparse import parse_datetime
+from rest_framework.decorators import action
+from django.db.models import Q
+from rest_framework.response import Response
+from datetime import datetime
 # Create your views here.
 
 class doctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    
+    @action(detail=False, methods=['GET'])
+    def searchDoctor(self, request):
+        keyword = request.query_params.get('keyword', None)
+        
+        if keyword is not None:
+            doctors = Doctor.objects.filter(Q(doctorName__icontains=keyword) | Q(department_selfPay__icontains=keyword) | Q(hospital__icontains=keyword))
+            if not doctors.exists():
+                return Response({"키워드에 해당하는 의사를 찾을 수 없습니다. 다시 입력하세요."})
+        else:
+            doctors = Doctor.objects.all()
+        
+        serializer = self.get_serializer(doctors, many=True)
+        doctor_names = [doctor['doctorName'] for doctor in serializer.data]
+        return Response(doctor_names)
